dea0d16b6c6542f007204af45a37a14f0462d4d9
Gary <g.wilson@parcel2go.com>
2018-04-24 21:59:48 +0000

    Add base64 function

--- src/CfSharp/Functions.cs
+++ src/CfSharp/Functions.cs
@@ -29,8 +29,27 @@ namespace CfSharp
             return new FnGetAtt(resource.GetName(), attributeName);
         }
 
+        public static FnBase64 Base64(object value)
+        {
+            return new FnBase64(value);
+        }
 
+    }
 
+    public class FnBase64
+    {
+        public FnBase64(string str)
+        {
+            Value = str;
+        }
+
+        public FnBase64(object obj)
+        {
+            Value = obj;
+        }
+
+        [JsonProperty(PropertyName = "Fn::Base64")]
+        public object Value { get; set; }
     }
 
     public class FnJoin
